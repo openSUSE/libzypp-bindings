af7bd1693171b6929e210e1294e3d72d46440d23
Arvin Schnell <aschnell@suse.de>
2007-07-24 09:39:45 +0000

    - generate python bindings

--- libzypp-bindings.spec.cmake
+++ libzypp-bindings.spec.cmake
@@ -15,7 +15,7 @@ License:        GPL
 Summary:        Bindings for libzypp
 Group:          Development/Sources
 BuildRoot:      %{_tmppath}/%{name}-%{version}-build
-BuildRequires:  cmake gcc-c++ ruby-devel swig
+BuildRequires:  cmake gcc-c++ python-devel ruby-devel swig
 BuildRequires:  libzypp-devel >= 3.0.0
 Source:         %{name}-%{version}.tar.bz2
 
@@ -54,5 +54,17 @@ Group:          Development/Languages/Ruby
 %files -n ruby-zypp
 %defattr(-,root,root,-)
 %{_libdir}/ruby/%{rb_ver}/%{rb_arch}/rzypp.so
+
+%package -n python-zypp
+Summary:        Python bindings for libzypp
+Group:          Development/Languages/Python
+
+%description -n python-zypp
+-
+
+%files -n python-zypp
+%defattr(-,root,root,-)
+%{_libdir}/python2.5/site-package/_zypp.so
+%{_libdir}/python2.5/site-package/zypp.py
 
 %changelog
