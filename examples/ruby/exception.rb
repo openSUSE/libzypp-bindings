ac6d55955bf9cf5cbd0f462cc30a05871a9f6c6f
Arvin Schnell <aschnell@suse.de>
2007-08-13 19:01:13 +0000

    - added exception example

---
+++ examples/ruby/exception.rb
@@ -0,0 +1,34 @@
+
+require 'rzypp'
+include Rzypp
+
+tmp_cache_path = TmpDir.new()
+tmp_raw_cache_path = TmpDir.new()
+tmp_known_repos_path = TmpDir.new()
+
+opts = RepoManagerOptions.new()
+opts.repoCachePath = tmp_cache_path.path()
+opts.repoRawCachePath = tmp_raw_cache_path.path()
+opts.knownReposPath = tmp_known_repos_path.path()
+
+repo_manager = RepoManager.new(opts)
+
+repo_info = RepoInfo.new()
+
+repo_info.set_alias("factorytest")
+repo_info.set_name("Test Repo for Factory.")
+repo_info.set_enabled(true)
+repo_info.set_autorefresh(false)
+repo_info.add_base_url("file:///tmp/does-not-exist")
+
+begin
+  repo_manager.add_repository(repo_info)
+  repo_manager.refresh_metadata(repo_info)
+  repo_manager.build_cache(repo_info)
+rescue ZYppException => e
+  puts "ZYppException caught"
+  puts e.to_s
+else
+  puts "Oh, no exception"
+end
+
