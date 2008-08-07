ee8254a71d55e471d386640fa193c2ce046ceeab
Klaus Kaempf <kkaempf@suse.de>
2008-08-07 16:09:10 +0000

    add mb example

--- swig/ruby/tests/bytecount.rb
+++ swig/ruby/tests/bytecount.rb
@@ -12,13 +12,14 @@ class LoadTest < Test::Unit::TestCase
   require 'zypp'
   include Zypp
 
-  def test_loading
-
+  def test_bytecount
     g = ByteCount.new(ByteCount.G)
     assert g
     gb = ByteCount.new(ByteCount.GB)
     assert gb
     k = ByteCount.new(ByteCount.K)
     assert k.to_i == 1024
+    mb = ByteCount.new(ByteCount.MB)
+    assert mb.to_i == 1000*1000
   end
 end
