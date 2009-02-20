2ec97ca1930673f6dbf424678024393e349fb9b9
Michael Andres <ma@suse.de>
2009-02-20 14:58:45 +0100

    fix outdated python examples

--- examples/python/install_updates_dryrun.py
+++ examples/python/install_updates_dryrun.py
@@ -8,50 +8,46 @@ except ImportError:
     print 'Dummy Import Error: Unable to import zypp bindings'
 
 print 'Reading repositories...'
-
 Z = zypp.ZYppFactory_instance().getZYpp()
-
 Z.initializeTarget( zypp.Pathname("/") )
-Z.addResolvables( Z.target().resolvables(), True )
+Z.target().load();
 
 repoManager = zypp.RepoManager()
 repos = repoManager.knownRepositories()
 
 for repo in repos:
-    if repo.enabled() and repo.autorefresh():
-        try:
-            repoManager.refreshMetadata(repo, zypp.RepoManager.RefreshIfNeeded) # or RefreshIfNeeded == 0
-        except:
-            repoManager.buildCache( repo )
-
-    Z.addResolvables( repoManager.createFromCache( repo ).resolvables())
-
-Z.applyLocks()
-Z.resolver().establishPool();
+    if not repo.enabled():
+        continue
+    if not repoManager.isCached( repo ):
+        repoManager.buildCache( repo )
+    repoManager.loadFromCache( repo );
+print "Items: %d" % ( Z.pool().size() )
 
 #
 # Does not to check and apply updates for the update stack first.
 #
-print 'List Upadtes:'
-for item in Z.pool().byKindIterator(zypp.KindOfPatch()):
-   if item.status().isInstalled():
-      continue
-   if item.status().isNeeded():
-      if not item.status().setTransact( True, zypp.ResStatus.USER ):
-        raise "Error set transact: %s" % item
-      resolvable = zypp.asKindPatch( item )
-      print '%s | %s-%s | %s | %s' % (resolvable.repository().info().alias(), resolvable.name(), resolvable.edition(), resolvable.category(), item.status() )
+Z.resolver().resolvePool()
+
+for item in Z.pool():
+  if not zypp.isKindPatch( item ):
+    continue
+  if item.isBroken():
+    if not item.status().setTransact( True, zypp.ResStatus.USER ):
+      raise "Error set transact: %s" % item
+    resolvable = zypp.asKindPatch( item )
+    print '%s | %s-%s | %s | %s' % (resolvable.repoInfo().alias(), resolvable.name(), resolvable.edition(), resolvable.category(), item.status() )
 
 if not Z.resolver().resolvePool():
   raise "Solver Error"
 
 for item in Z.pool():
   if item.status().transacts():
-    print item
+    print '%s | %s-%s | %s' % (item.repoInfo().alias(), item.name(), item.edition(), item.status() )
 
 #
 # dryRun!
 #
+
 policy = zypp.ZYppCommitPolicy()
 policy.dryRun( True )
 policy.syncPoolAfterCommit( False )
