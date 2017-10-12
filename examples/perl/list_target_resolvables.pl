0581dcbaf61296696ab34e4e35366f105e39e860
Michael Andres <ma@suse.de>
2017-10-12 12:39:15 +0200

    by V. Zhestkov: Fix perl examples

--- examples/perl/list_target_resolvables.pl
+++ examples/perl/list_target_resolvables.pl
@@ -5,18 +5,32 @@ $zypp = $z->getZYpp;
 
 $t = $zypp->initializeTarget(zypp::Pathname->new("/"));
 
-$repo = $zypp->target;
-$store = $repo->resolvables;
+my $repoManager = zypp::RepoManager::new();
+my $repos = zypp::RepoManager::knownRepositories($repoManager);
+
+foreach $repo ( @{$repos} ) {
+	if ( $repo->enabled() ) {
+		print("Caching repo: ".$repo->name."\n");
+		if ( ! zypp::RepoManager::isCached($repoManager,$repo) ) {
+			print("Rebuilding cache for: ".$repo->name."\n");
+			zypp::RepoManager::buildCache($repoManager,$repo);
+		}
+		zypp::RepoManager::loadFromCache($repoManager,$repo);
+	}
+}
+
+$store = $zypp->pool;
 
 $it_b = $store->cBegin;
+$it_e = $store->cEnd;
 
-while ($store->iterator_equal($it_b, $store->cEnd) ne 1){
+while ($store->iterator_equal($it_b, $it_e) ne 1){
    $pkg = $store->iterator_value($it_b);
-   print $pkg->kindToS, " ", $pkg->name, " ", $pkg->edition->asString;
-   print $pkg->arch->asString, "\n";
+   print $pkg->kind->asString, " ", $pkg->name, " ", $pkg->edition->asString;
+   print $pkg->arch->string, "\n";
    print "  Summary: ", $pkg->summary, "\n";
-   print "  Size: ", $pkg->size, "\n";
-   print "  Vendor: ", $pkg->vendor, "\n";
+   print "  Size: ", $pkg->installSize->asString, "\n";
+   print "  Vendor: ", $pkg->vendor->asString, "\n";
    print "  BuildTime: ", $pkg->buildtime->asString, "\n";
    $it_b = $store->iterator_incr($it_b);
 
